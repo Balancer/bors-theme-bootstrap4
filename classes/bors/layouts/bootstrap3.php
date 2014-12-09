ad3f43367d6c9e53de6acbfd8ba38b48f42f45d6
Balancer <balancer@balancer.ru>
2014-12-09 06:29:59 +0300

    Унифицированные формы bootstrap3.

--- classes/bors/layouts/bootstrap3.php
+++ classes/bors/layouts/bootstrap3.php
@@ -4,4 +4,6 @@ class bors_layouts_bootstrap3 extends bors_layouts_html
 {
 	function table_class() { return 'table table-bordered table-striped'; }
 	function ul_tab_class() { return 'nav nav-tabs'; }
+
+	function forms_template_class() { return 'bors_layouts_bootstrap3_forms'; }
 }
---
+++ classes/bors/layouts/bootstrap3/forms.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+	Параметры для форм по умолчанию
+*/
+
+class bors_layouts_bootstrap3_forms extends bors_forms_templates_default
+{
+	function _form_table_css_def() { return 'table'; }
+	function _form_table_left_th_css_def() { return 'span4'; }
+
+	function _input_css_def() { return 'form-control'; }
+	function _input_css_error_def() { return 'alert'; }
+	function _textarea_css_def() { return 'span8'; }
+
+	function _dropdown_css_def() { return 'span8'; }
+
+	function _submit_css_def() { return 'btn'; }
+
+	function _form_container_html_def() { return "<div class=\"well container\">\n%s</div>\n"; }
+	function _form_row_html_def() { return "<div class=\"row\">%s</div>\n"; }
+	function _form_element_label_html_def() { return "<div class=\"span2\">%s</div>"; }
+	function _form_element_html_def() { return "<div class=\"span8\">%s</div>"; }
+}
