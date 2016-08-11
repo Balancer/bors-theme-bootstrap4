e4b41189ee3766effcbbef39ad3cde353251c5e7
Balancer <balancer@balancer.ru>
2016-08-11 17:18:02 +0300

    ++

--- classes/bors/layouts/bootstrap3/forms.php
+++ classes/bors/layouts/bootstrap3/forms.php
@@ -11,14 +11,14 @@ class bors_layouts_bootstrap3_forms extends bors_forms_templates_default
 
 	function _input_css_def() { return 'form-control'; }
 	function _input_css_error_def() { return 'alert'; }
-	function _textarea_css_def() { return 'span8'; }
+	function _textarea_css_def() { return 'form-control'; }
 
 	function _dropdown_css_def() { return 'span8'; }
 
 	function _submit_css_def() { return 'btn'; }
 
 	function _form_container_html_def() { return "<div class=\"well container\">\n%s</div>\n"; }
-	function _form_row_html_def() { return "<div class=\"row\">%s</div>\n"; }
-	function _form_element_label_html_def() { return "<div class=\"span2\">%s</div>"; }
-	function _form_element_html_def() { return "<div class=\"span8\">%s</div>"; }
+	function _form_row_html_def() { return "<div class=\"form-group\">\n%s\n%s</div>\n"; }
+	function _form_element_label_html_def() { return "\t<label>%s</label>"; }
+	function _form_element_html_def() { return "\t%s"; }
 }
