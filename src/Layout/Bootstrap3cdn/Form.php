4d60d61f891b224bdc20be2f81a3e9d2fc26edf5
Balancer <balancer@balancer.ru>
2017-01-22 09:02:23 +0300

    Update and fix

--- src/Layout/Bootstrap3cdn/Form.php
+++ src/Layout/Bootstrap3cdn/Form.php
@@ -15,14 +15,24 @@ class Form extends \B2\Layout\Common\Form
 
 	function _input_css_def() { return 'form-control'; }
 	function _input_css_error_def() { return 'alert'; }
-	function _textarea_css_def() { return 'span8'; }
+	function _textarea_css_def() { return 'form-control'; }
 
 	function _dropdown_css_def() { return 'span8'; }
 
 	function _submit_css_def() { return 'btn'; }
 
-	function _form_container_html_def() { return "<div class=\"well container\">\n%s</div>\n"; }
-	function _form_row_html_def() { return "<div class=\"row\">%s</div>\n"; }
-	function _form_element_label_html_def() { return "<div class=\"span2\">%s</div>"; }
-	function _form_element_html_def() { return "<div class=\"span8\">%s</div>"; }
+	// Common form outer html.
+	function _form_container_html_def() { return "%s"; }
+
+	// Form title
+	function _form_title_html_def() { return "<legend>%s</legend>"; }
+
+	// content inner "<form>...</form>", two arguments: label and fields
+	function _form_content_html_def() { return "<fieldset>\n%s\n%s</fieldset>"; }
+
+	// One form row: $element_title and $element_html
+	function _form_row_html_def() { return "\t<div class=\"form-group\">\n\t\t%s\n\t\t%s\n\t</div>\n"; }
+
+	function _form_element_label_html_def() { return "\t\t<label>\t\t\t%s\t\t</label>\n"; }
+	function _form_element_html_def() { return "\t\t%s\n"; }
 }
--- src/Theme/Bootstrap3cdn.tpl.php
+++ src/Theme/Bootstrap3cdn.tpl.php
@@ -122,10 +122,8 @@ else
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-	<script src="../../assets/js/docs.min.js"></script>
 	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-	<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/js/ie10-viewport-bug-workaround.js"></script>
 
 <?php
 
