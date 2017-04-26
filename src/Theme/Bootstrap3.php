3d581e82e3f3f49d09cd5a3a6496cd243e0bf0e2
Balancer <balancer@balancer.ru>
2017-04-26 10:26:17 +0300

    Default bootstrap3 theme can be used without asset.

--- src/Theme/Bootstrap3.php
+++ src/Theme/Bootstrap3.php
@@ -11,22 +11,25 @@ class Bootstrap3 extends \B2\Theme\Common
 		// Bootstrap's JavaScript requires jQuery
 		\B2\jQuery::load();
 
-		if(empty(\bors::$bower_asset_packages['bower-asset/bootstrap']))
+		if(empty($this->get('__skip_pure_bootstrap_load')))
 		{
-			bors_use('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
-			bors_use('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css');
-			bors_use('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
-			// IE10 viewport hack for Surface/desktop Windows 8 bug
-		}
-		else
-		{
-			$asset = \B2\cfg('bower-asset.path', '/bower-asset').'/bootstrap/dist';
-			bors_use($asset.'/css/bootstrap.min.css');
-			bors_use($asset.'/css/bootstrap-theme.min.css');
-			bors_use($asset.'/js/bootstrap.min.js');
-		}
+			if(empty(\bors::$bower_asset_packages['bower-asset/bootstrap']))
+			{
+				bors_use('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
+				bors_use('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css');
+				bors_use('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
+				// IE10 viewport hack for Surface/desktop Windows 8 bug
+			}
+			else
+			{
+				$asset = \B2\cfg('bower-asset.path', '/bower-asset').'/bootstrap/dist';
+				bors_use($asset.'/css/bootstrap.min.css');
+				bors_use($asset.'/css/bootstrap-theme.min.css');
+				bors_use($asset.'/js/bootstrap.min.js');
+			}
 
-		bors_use('//maxcdn.bootstrapcdn.com/js/ie10-viewport-bug-workaround.js');
+			bors_use('//maxcdn.bootstrapcdn.com/js/ie10-viewport-bug-workaround.js');
+		}
 
 		return parent::pre_show();
 	}
