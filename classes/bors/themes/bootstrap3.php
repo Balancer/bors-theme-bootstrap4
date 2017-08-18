b6d1e9eb6dcdb05b07164d4bb95280ea09b24d1f
Balancer <balancer@balancer.ru>
2017-08-18 11:26:12 +0300

    theme fix

--- classes/bors/themes/bootstrap3.php
+++ classes/bors/themes/bootstrap3.php
@@ -1,6 +1,6 @@
 <?php
 
-class bors_themes_bootstrap3 extends B2\Theme\Common
+class bors_themes_bootstrap3 extends \B2\Theme\Bootstrap3
 {
-	var $layout_class = 'bors_layouts_bootstrap3';
+	var $layout_class = \B2\Layout\Bootstrap3::class;
 }
