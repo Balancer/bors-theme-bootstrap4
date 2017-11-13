f2b8093ea37130859577319774da1e97b91c2d21
Balancer <balancer@balancer.ru>
2017-11-13 04:04:41 +0300

    Move of the asset loader to a separate `bors/bootstrap3` package.

--- src/Theme/Bootstrap3.php
+++ src/Theme/Bootstrap3.php
@@ -12,24 +12,7 @@ class Bootstrap3 extends \B2\Theme\Common
 		\B2\jQuery::load();
 
 		if(empty($this->get('__skip_pure_bootstrap_load')))
-		{
-			if(empty(\bors::$bower_asset_packages['bower-asset/bootstrap']))
-			{
-				bors_use('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
-				bors_use('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css');
-				bors_use('//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
-				// IE10 viewport hack for Surface/desktop Windows 8 bug
-			}
-			else
-			{
-				$asset = \B2\cfg('bower-asset.path', '/bower-asset').'/bootstrap/dist';
-				bors_use($asset.'/css/bootstrap.min.css');
-				bors_use($asset.'/css/bootstrap-theme.min.css');
-				bors_use($asset.'/js/bootstrap.min.js');
-			}
-
-			bors_use('//maxcdn.bootstrapcdn.com/js/ie10-viewport-bug-workaround.js');
-		}
+			\B2\Bootstrap3::load();
 
 		return parent::pre_show();
 	}
