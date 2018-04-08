900103b34f1f3cd3cebaacea654dfe944f0b431d
Balancer <balancer@aviaport.lxd>
2018-04-08 07:53:11 +0300

    Support custom CSS classes for buttons

--- src/Layout/Bootstrap3/Button.php
+++ src/Layout/Bootstrap3/Button.php
@@ -12,6 +12,9 @@ class Button extends \B2\Layout\Module
 		if($fa = $this->arg('fa'))
 			$label = "<i class=\"fa fa-$fa\"></i> $label";
 
+		if($icon_class = $this->arg('icon-class'))
+			$label = "<i class=\"{$icon_class}\"></i> $label";
+
 		return "<a href=\"$url\" class=\"btn btn-default\">{$label}</a>";
 	}
 }
